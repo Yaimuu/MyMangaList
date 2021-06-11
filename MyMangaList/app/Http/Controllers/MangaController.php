15b2e9ef3b2ad25a26baf3ba9a066239d69dfc0c
AHMED KHAN Somir <somir.ahmed-khan@etu.univ-lyon1.fr>
2021-06-11 10:18:42 +0200

    insert into dans projet.sql

--- MyMangaList/app/Http/Controllers/MangaController.php
+++ MyMangaList/app/Http/Controllers/MangaController.php
@@ -10,24 +10,27 @@ class MangaController extends Controller
 {
     public function index()
     {
-        $mangas = DB::table('Manga')->get();
+        // $mangas = DB::table('Manga')->get();
+        
+        $mangas = Manga::all();
+
         return view('mangas', compact('mangas'));
 
     }
 
     public function show(Manga $manga)
     {
-        // $tomes = DB::table('Tome')->where('Id_Manga',$manga->Id_Manga)->get();
-        // $createurs = DB::table('Creer')->where('Id_Manga', $manga->Id_Manga)->get();
+        $tomes = DB::table('Tome')->where('Id_Manga',$manga->Id_Manga)->get();
+        $createurs = DB::table('Creer')->where('Id_Manga', $manga->Id_Manga)->get();
         
  
-        // foreach ($createurs as $createur) {
-        //     $auteur = DB::table('Auteur')->where('Id_Auteur', $createur->Id_Auteur)->first();
-        //     $artiste_auteur = DB::table('Artiste')->where('Id_Artiste', $auteur->Id_Artiste)->first();
+        foreach ($createurs as $createur) {
+            $auteur = DB::table('Auteur')->where('Id_Auteur', $createur->Id_Auteur)->first();
+            $artiste_auteur = DB::table('Artiste')->where('Id_Artiste', $auteur->Id_Artiste)->first();
 
-        //     $dessinateur = DB::table('Dessinateur')->where('Id_Dessinateur', $createur->Id_Dessinateur)->first();
-        //     $artiste_dessinateur = DB::table('Artiste')->where('Id_Artiste', $createur->Id_Artiste)->first();
-        // }
+            $dessinateur = DB::table('Dessinateur')->where('Id_Dessinateur', $createur->Id_Dessinateur)->first();
+            $artiste_dessinateur = DB::table('Artiste')->where('Id_Artiste', $createur->Id_Artiste)->first();
+        }
         return view('manga', compact('manga'));       
     }
 }
