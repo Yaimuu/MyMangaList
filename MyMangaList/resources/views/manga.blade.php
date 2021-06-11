f06009931facc306def601998c033c186ba5592e
adrbe <adr.bertrand@gmail.com>
2021-06-11 12:07:56 +0200

    debut page manga

--- MyMangaList/resources/views/manga.blade.php
+++ MyMangaList/resources/views/manga.blade.php
@@ -8,10 +8,12 @@
     <div class="card">
         <header class="card-header">
             <h5 class="card-header-title">Titre : {{ $manga->titre }}</h5>
+            <p>Auteur : {{$artiste_auteur->prenom}}  {{$artiste_auteur->nom}}</p>
+            <p>Dessinateur : {{$artiste_dessinateur->prenom}}  {{$artiste_dessinateur->nom}}</p>
             @foreach($tomes as $tome)
                 <img src="{{$tome->image}}" alt="{{$tome->nom}}" />
             @endforeach
-            <p>Nom auteur :</p>
+            
         </header>
         {{-- <div class="card-content">
             <div class="content">
