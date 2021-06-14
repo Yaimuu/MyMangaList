5475b7b03b65371fe77c59f3e5b18fb7a9d4358a
Yaimuu <yanis.ouled@gmail.com>
2021-06-14 17:20:31 +0200

    feature: Authentification

--- MyMangaList/app/Utilisateur.php
+++ MyMangaList/app/Utilisateur.php
@@ -3,8 +3,40 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class Utilisateur extends Model
 {
-    //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'pseudo', 'mail', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    protected $table = 'Utilisateur';
 }
